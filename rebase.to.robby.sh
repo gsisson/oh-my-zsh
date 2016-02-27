b47159900f84995cfd03e78ad1e3c1194236fc26
Glenn Sisson <glenn.sisson@gmail.com>
2016-02-27 13:46:22 -0800

    add "rebase.to.robby.sh" script

--- rebase.to.robby.sh
+++ rebase.to.robby.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-rebase.to.forked.repo.sh robby # --repo robbyrussell/oh-my-zsh
+# rebase.to.forked.repo.sh robby # --repo robbyrussell/oh-my-zsh
+  rebase.to.forked.repo.sh robby   --repo robbyrussell/oh-my-zsh
